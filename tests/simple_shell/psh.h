43422b5e12bc2710972465da20be34a3b863b73a
praton1729 <prathu.baronia@oneplus.com>
2021-04-20 00:53:09 +0530

    Add a test psh shell example
    - This was just an experiment.
    - PSH can be integrated into this when it matures.
    
    Signed-off-by: praton1729 <prathu.baronia@oneplus.com>

---
+++ tests/simple_shell/psh.h
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  psh.h
+ *
+ *    Description:  Interface to praton shell routines
+ *
+ *        Version:  1.0
+ *        Created:  Monday 19 April 2021 01:37:51  IST
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  PRATHU BARONIA (praton), prathu.baronia@oneplus.com
+ *   Organization:  OnePlus RnD
+ *
+ * =====================================================================================
+ */
+
+void psh_loop(void);
